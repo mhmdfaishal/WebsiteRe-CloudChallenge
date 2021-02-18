156fed1762cc67a6adcf181bd37c8e6a9716e92a
mhmdfaishal <mhmd.faishal123@gmail.com>
2021-02-18 17:40:50 +0700

    Web Re-Cloud Challenge

--- campou.sql
+++ campou.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2020 at 09:31 AM
+-- Generation Time: Nov 26, 2020 at 03:32 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -39,9 +39,7 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`username_cust`, `password`, `Email`, `foto`) VALUES
-('mhmdfaishal12', '$2y$10$3hZC1m/Ble2xMLMNmVLMDOJiWiO94hGmNU8Wieg5crWlr.KVkS8l6', 'mhmd.faishal123@gmail.com', '1607357198_233163ac3d542daa5ee5.png'),
-('test12', '$2y$10$r9o.0r6sPQE2XmYOEVdu8e7xhpu3X7DhsLOKeGsoyCvM08NYCLB/a', 'mhmd.faishal12@gmail.com', '1607500677_ffd9bb9bb8102421e763.png'),
-('test123', '$2y$10$Dt2Y4DeBIGEsrYBYDcaB6eiAcEiiPhDpSPoazYukTi6nwBEay3jJi', 'tes1@gmail.com', '1607436334_9aeb62c4374f0e7f470b.jpg');
+('faishal', '$2y$10$JVfSh1xdV6P8m1Em99vx2.2Y43j6K0KIZPvdLp2VXeRc1aEXqTdT6', 'mhmd.faishal12@gmail.com', '1606269690_762b55d2e7f649a7113a.png');
 
 -- --------------------------------------------------------
 
@@ -55,7 +53,7 @@ CREATE TABLE `detail_penyewaan` (
   `Nama` varchar(30) NOT NULL,
   `Nomor_Telepon` char(12) NOT NULL,
   `Nama_Lapangan` varchar(30) NOT NULL,
-  `IdLapangan` varchar(4) DEFAULT NULL,
+  `IdLapangan` varchar(4) NOT NULL,
   `tgl_main` date NOT NULL,
   `jam_mulai` time NOT NULL,
   `jam_selesai` time NOT NULL,
@@ -68,7 +66,26 @@ CREATE TABLE `detail_penyewaan` (
 --
 
 INSERT INTO `detail_penyewaan` (`nomor_penyewaan`, `username_cust`, `Nama`, `Nomor_Telepon`, `Nama_Lapangan`, `IdLapangan`, `tgl_main`, `jam_mulai`, `jam_selesai`, `total_harga`, `durasi`) VALUES
-('2851', 'test12', 'test', '0897664564', 'Lapangan Futsal 01', 'LP1', '2020-12-10', '14:00:00', '16:00:00', 130000, 2);
+('5742', 'faishal', 'Aku Anak Baik', '0876582854', 'Lapangan Futsal', 'LP1', '2020-11-30', '13:00:00', '14:00:00', 65000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `kode_jadwal` varchar(4) NOT NULL,
+  `jam` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jadwal`
+--
+
+INSERT INTO `jadwal` (`kode_jadwal`, `jam`) VALUES
+('jd1', '24'),
+('jd2', '24');
 
 -- --------------------------------------------------------
 
@@ -80,8 +97,8 @@ CREATE TABLE `lapangan` (
   `IdLapangan` varchar(4) NOT NULL,
   `Nama_Lapangan` varchar(25) NOT NULL,
   `kode_tarif` varchar(4) NOT NULL,
+  `kode_jadwal` varchar(4) NOT NULL,
   `Nomor_Lapangan` varchar(5) NOT NULL,
-  `deskripsi` varchar(255) NOT NULL,
   `foto` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -89,11 +106,10 @@ CREATE TABLE `lapangan` (
 -- Dumping data for table `lapangan`
 --
 
-INSERT INTO `lapangan` (`IdLapangan`, `Nama_Lapangan`, `kode_tarif`, `Nomor_Lapangan`, `deskripsi`, `foto`) VALUES
-('LP1', 'Lapangan Futsal2', 'trf2', '01', 'Lapangan Futsal dengan ukuran dan fasilitas standard internasional', '1607437683_cb3cf4ae8ee88da71f6a.jpg'),
-('LP2', 'Lapangan Basket', 'trf2', '01', 'Lapangan voly dengan ukuran dan fasilitas standard internasional', 'basket1.jpg'),
-('LP3', 'Lapangan Voly', 'trf1', '01', 'Lapangan basket dengan ukuran dan fasilitas standard internasional', 'voly1.jpg'),
-('LP4', 'Lapangan Futsal', 'trf1', '02', 'tes', '1607501071_d7a90c58e69e0eb1e98b.jpg');
+INSERT INTO `lapangan` (`IdLapangan`, `Nama_Lapangan`, `kode_tarif`, `kode_jadwal`, `Nomor_Lapangan`, `foto`) VALUES
+('LP1', 'Lapangan Futsal', 'trf1', 'jd1', '01', 'futsal1.jpg'),
+('LP2', 'Lapangan Basket', 'trf2', 'jd2', '01', 'basket1.jpg'),
+('LP3', 'Lapangan Voly', 'trf1', 'jd1', '01', 'voly1.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,9 +131,9 @@ CREATE TABLE `penyedia` (
 --
 
 INSERT INTO `penyedia` (`IdPenyedia`, `Nama_Penyedia`, `email`, `foto`, `password`, `Nomor_Telepon`) VALUES
-('0001', 'Faishal', 'faishal@gmail.com', '16.jpg', '$2y$10$U/KMa0GM.PCFKK8xo0NKeOO5tIw201tGXDpQxWBcRZcZ04AW3x/xu', '082127177132'),
-('0002', 'Join', 'join@gmail.com', '1607143741_fb26c068048797d0c2d0.jpg', '$2y$10$8eB3O2OzgussEdkCr/.quuX9VrMZhlChFX/HZV.G57p5AYdnr43K2', '08927372324'),
-('0003', 'ridho', 'ridho@gmail.com', '44.jpg', '$2y$10$OnFSm7l0iXUNihMGeamE/urDzXjOf5sROHkrOkLxA47FkNn.DkZeG', '07887867868');
+('0001', 'Faishal', 'alexander@gmail.com', '16.jpg', '$2y$10$KZfm0mcZI757ZT/TOcENW.mkRLSlb3AgnAUZwJEE3nEFht83xUkDi', '087361826218'),
+('0002', 'Join', 'join@gmail.com', '20.jpg', '$2y$10$Om8lR4et4.CQmtpEhk.pquB7xE0LZeWIBdhLlNwRgY.FKoV0G1LlC', '08646732612'),
+('0020', 'Ridho', 'ridho@gmail.com', '44.jpg', '$2y$10$asN.Oi4WcAjVVYLQUm8yae9O3WX.A9qIGb5rN1.vlludn27qM1jDC', '087632532732');
 
 -- --------------------------------------------------------
 
@@ -157,10 +173,17 @@ ALTER TABLE `detail_penyewaan`
   ADD KEY `username_cust` (`username_cust`);
 
 --
+-- Indexes for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`kode_jadwal`);
+
+--
 -- Indexes for table `lapangan`
 --
 ALTER TABLE `lapangan`
   ADD PRIMARY KEY (`IdLapangan`),
+  ADD KEY `kode_jadwal` (`kode_jadwal`),
   ADD KEY `kode_tarif` (`kode_tarif`);
 
 --
@@ -185,6 +208,13 @@ ALTER TABLE `tarif`
 ALTER TABLE `detail_penyewaan`
   ADD CONSTRAINT `IdLapangan` FOREIGN KEY (`IdLapangan`) REFERENCES `lapangan` (`IdLapangan`),
   ADD CONSTRAINT `username_cust` FOREIGN KEY (`username_cust`) REFERENCES `customer` (`username_cust`);
+
+--
+-- Constraints for table `lapangan`
+--
+ALTER TABLE `lapangan`
+  ADD CONSTRAINT `kode_jadwal` FOREIGN KEY (`kode_jadwal`) REFERENCES `jadwal` (`kode_jadwal`),
+  ADD CONSTRAINT `kode_tarif` FOREIGN KEY (`kode_tarif`) REFERENCES `tarif` (`kode_tarif`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
